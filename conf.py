1c9e0cb1758612c4447e2a876ad7661c9eacb9b4
ilker manap <ilkermanap@gmail.com>
2021-09-12 21:27:25 +0200

    conf.py icinde duzenleme

--- conf.py
+++ conf.py
@@ -206,6 +206,7 @@ htmlhelp_basename = 'PyQtvePySidernekleridoc'
 latex_elements = {
 'preamble': r'''
 \usepackage{hyperref}
+\usepackage[T1]{fontenc} 
 ''',
 # The paper size ('letterpaper' or 'a4paper').
 #'papersize': 'letterpaper',
@@ -224,7 +225,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'PyQtvePySidernekleri.tex', u'PyQt ve PySide Örnekleri Documentation',
+    (master_doc, 'PyQtvePySidernekleri.tex', u'PyQt ve PySide Örnekleri',
      u'Ilker Manap', 'manual'),
 ]
 
@@ -254,7 +255,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'pyqtvepysidernekleri', u'PyQt ve PySide Örnekleri Documentation',
+    (master_doc, 'pyqtvepysidernekleri', u'PyQt ve PySide Örnekleri',
      [author], 1)
 ]
 
@@ -268,7 +269,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'PyQtvePySidernekleri', u'PyQt ve PySide Örnekleri Documentation',
+    (master_doc, 'PyQtvePySidernekleri', u'PyQt ve PySide Örnekleri',
      author, 'PyQtvePySidernekleri', 'One line description of project.',
      'Miscellaneous'),
 ]
